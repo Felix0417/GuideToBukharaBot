c0547029ec7b3b6be3dbdcf412c39a06fe7ac961
theTh1rt3en <ipolyakov@swordfishsecurity.com>
2022-12-14 00:40:22 +0600

    fixed soe code and added comments

--- src/main/java/com/telegramBots/GuideToBukharaBot/service/LoadLeftMenu.java
+++ src/main/java/com/telegramBots/GuideToBukharaBot/service/LoadLeftMenu.java
@@ -1,36 +1,41 @@
 package com.telegramBots.GuideToBukharaBot.service;
 
 import com.telegramBots.GuideToBukharaBot.model.MenuButtonTags;
+import io.vavr.control.Try;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+
 @Slf4j
+@Component
+@RequiredArgsConstructor
 public class LoadLeftMenu {
 
-    TelegramBot bot;
+    //private final заставит сгенерить аннотацию @ReqArgsCons с этим полем. Спринг автоматом инжектит бины, если конструктор
+    //один, поэтому дополнительных вещей типа (onConstructor = @__(@Autowired)) не надо добавлять
+    private final TelegramBot bot;
+    private final List<BotCommand> listOfCommands = new ArrayList<>();
 
-//    @PostConstruct
-    private void loadMenu(){
-        List<BotCommand> listOfCommands = new ArrayList<>();
+    @PostConstruct
+    private void loadMenu() {
+
         listOfCommands.add(new BotCommand(MenuButtonTags.START.getCommand(), MenuButtonTags.START.getDescription()));
         listOfCommands.add(new BotCommand(MenuButtonTags.USER_DATA.getCommand(), MenuButtonTags.USER_DATA.getDescription()));
         listOfCommands.add(new BotCommand(MenuButtonTags.HELP.getCommand(), MenuButtonTags.HELP.getDescription()));
         listOfCommands.add(new BotCommand(MenuButtonTags.ABOUT_BOT.getCommand(), MenuButtonTags.ABOUT_BOT.getDescription()));
         listOfCommands.add(new BotCommand(MenuButtonTags.SETTINGS.getCommand(), MenuButtonTags.SETTINGS.getDescription()));
 
-        try {
-            bot.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), "ru"));
-        } catch (TelegramApiException e) {
-            log.error("Error setting's bot command list: " + e.getMessage());
-        }
+        //немножко облагородим код, избавив его от старых обработок эксепшенов. в идеале эксепшенов быть не должно
+        Try.of(() -> bot.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), "ru")))
+                .onFailure(e -> log.error("Error setting's bot command list: " + e.getMessage()))
+                .get();
     }
 }
