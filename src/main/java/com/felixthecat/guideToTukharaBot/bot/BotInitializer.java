da388483f2b6f06013c61518ec12e99f47255c9d
theTh1rt3en <ipolyakov@swordfishsecurity.com>
2023-01-02 23:51:25 +0500

    fix

---
+++ src/main/java/com/felixthecat/guideToTukharaBot/bot/BotInitializer.java
@@ -0,0 +1,32 @@
+package com.felixthecat.guideToTukharaBot.bot;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
+import org.telegram.telegrambots.meta.generics.TelegramBot;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class BotInitializer {
+
+    TelegramBot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+
+        try {
+            botsApi.registerBot((LongPollingBot) bot);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred " + e.getMessage());
+        }
+    }
+}
